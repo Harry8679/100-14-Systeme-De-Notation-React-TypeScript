afea6a1e24947d4a29a4a891e1bf62c0976cb0a0
Harry7986 <emarh.harry.code@gmail.com>
2026-02-20 17:22:08 +0100

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import './App.css'
+import { RatingSystem } from './components/RatingSystem';
 
 function App() {
-
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <RatingSystem />;
 }
 
-export default App
+export default App;
